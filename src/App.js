fbb735113676a3d6ba3ead63748ff238df4847de
Jonas Holbech <jofh@kea.dk>
2020-01-28 23:11:27 +0100

    hackish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   RGBToHex,
   RGBAToHexA,
@@ -33,8 +33,32 @@ const copyToClipboard = str => {
 };
 
 function App() {
-  const [input, setInput] = useState("lightblue");
+  const [input, setInput] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [activeSuggestion, setActiveSuggestion] = useState(-1);
+  useEffect(() => {
+    //TODO: brug ref til input
+    window.addEventListener("keyup", handleDropDown);
+    function handleDropDown(e) {
+      if (suggestions.length > 0) {
+        if (e.key === "ArrowDown") {
+          setActiveSuggestion(activeSuggestion + 1);
+        } else if (e.key === "ArrowUp") {
+          setActiveSuggestion(activeSuggestion - 1);
+        } else if (
+          e.key === "Enter" &&
+          activeSuggestion > -1 &&
+          activeSuggestion < suggestions.length
+        ) {
+          updateInputState(suggestions[activeSuggestion]);
+          setActiveSuggestion(-1);
+        }
+      }
+      return function cleanup() {
+        window.removeEventListener("keyup", handleDropDown);
+      };
+    }
+  }, [activeSuggestion, suggestions]);
   let colorHex, colorHexA, colorRgb, colorRgbA, colorHsl, colorHslA;
   const colorFormat = getColorFormat(input);
   switch (colorFormat) {
@@ -114,12 +138,17 @@ function App() {
         color => val !== "" && color.startsWith(val) && color !== val
       )
     );
+    setActiveSuggestion(-1);
   }
 
   return (
     <div id="wrapper">
       <div id="formWrapper">
-        <form>
+        <form
+          onSubmit={e => {
+            e.preventDefault(); //TODO: virker ikke
+          }}
+        >
           <input
             type="text"
             name="color"
@@ -130,7 +159,11 @@ function App() {
           <ul>
             {suggestions.map((sug, i) => {
               return (
-                <li key={i} onClick={() => updateInputState(sug)}>
+                <li
+                  key={i}
+                  className={activeSuggestion === i ? "active" : ""}
+                  onClick={() => updateInputState(sug)}
+                >
                   {sug}
                 </li>
               );
