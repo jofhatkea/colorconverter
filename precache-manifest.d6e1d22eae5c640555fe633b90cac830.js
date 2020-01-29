2a044ec264d056d0ac01de6beb90eb5be10ad64b
Jonas Holbech <jofh@kea.dk>
2020-01-29 12:23:08 +0100

    Updates

---
+++ precache-manifest.d6e1d22eae5c640555fe633b90cac830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac72087be766ca4596be7c2aea8d4f6",
+    "url": "/colorconverter/index.html"
+  },
+  {
+    "revision": "785f44ab2e986c32fdf8",
+    "url": "/colorconverter/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "d7e963148c1c4ac2a950",
+    "url": "/colorconverter/static/css/main.a0bccfda.chunk.css"
+  },
+  {
+    "revision": "785f44ab2e986c32fdf8",
+    "url": "/colorconverter/static/js/2.d0f149e1.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/colorconverter/static/js/2.d0f149e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7e963148c1c4ac2a950",
+    "url": "/colorconverter/static/js/main.485c2301.chunk.js"
+  },
+  {
+    "revision": "ea0cf30299e6285665ba",
+    "url": "/colorconverter/static/js/runtime-main.08d07612.js"
+  }
+]);
